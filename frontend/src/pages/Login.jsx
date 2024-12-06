7f4cd3d048047737e5f71bc4c8c1fc80fd5a4096
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-12-06 22:25:39 +0545

    added tailwind

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,47 +15,71 @@ function Login({ setLoggedin, email, password, setEmail, setPassword }) {
   };
 
   return (
-    <div className="app">
-      <h1>Login</h1>
-      <div className="form">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          style={{
-            padding: "8px",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-            marginRight: "8px",
-          }}
-        />
-        <input
-          type="password"
-          placeholder="Enter your password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{
-            padding: "8px",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-            marginRight: "8px",
-          }}
-        />
-        <br />
-        <br />
-        <button onClick={handleLogin}>Login</button>
+    <div className="relative min-h-screen flex items-center justify-center">
+      {/* Background video */}
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute inset-0 h-full w-full object-cover z-0"
+      >
+        <source src="../public/Assets/Videos/video5.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div
+        className="relative z-10 h-[550px] w-[500px] overflow-hidden rounded-3xl bg-white bg-opacity-10
+        backdrop-blur-md shadow-lg"
+      >
+        <div className="mt-10 space-y-8 px-10 py-10 text-center">
+          <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#3E2723] via-[#0D47A1] to-[#B71C1C] mb-6">
+            Nepali Radio
+          </h1>
+
+          <div className="group relative">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="peer h-14 w-full rounded-3xl bg-gray-100 px-4 mb-8 text-sm outline-none"
+            />
+            <label
+              className="absolute left-2 top-[-115px] flex h-full transform items-center pl-2 text-base transition-all duration-300 peer-placeholder-shown:top-4
+            peer-placeholder-shown:h-full peer-focus:-top-20 peer-focus:h-1/2 peer-focus:pl-0 peer-focus:text-white"
+            >
+              Email
+            </label>
+
+            <div className="group relative">
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="peer h-14 mb-8 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none"
+              />
+              <label className="absolute left-2 top-[-37px] flex h-full transform items-center pl-2 text-base transition-all duration-300 peer-placeholder-shown:top-2 peer-placeholder-shown:h-full peer-focus:-top-11 peer-focus:h-1/2 peer-focus:pl-0 peer-focus:text-white">
+                Password
+              </label>
+              <button
+                className="h-12 w-full rounded-3xl bg-blue-900 text-white transition-all duration-300 hover:bg-blue-800"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </div>
+            <p className="text-center mt-10 text-white gap-2">
+              {" "}
+              Don&apos;t have an account?
+              <a
+                onClick={() => setLoggedin(false)}
+                className="font-semibold text-blue-500 hover:text-blue-800 cursor-pointer"
+              >
+                {" "}
+                Signup
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
-      <br />
-      <br />
-      <span>
-        {" "}
-        Don&apos;t have an account?
-        <a onClick={() => setLoggedin(false)} style={{ cursor: "pointer" }}>
-          {" "}
-          Signup
-        </a>
-      </span>
     </div>
   );
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -27,51 +27,90 @@ function Signup({ email, password, setEmail, setPassword, setLoggedin }) {
 
   return (
     <>
-      <div className="app">
-        <h1>Sign Up</h1>
-        <div className="form">
-          <input
-            type="email"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            style={{
-              padding: "8px",
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-              marginRight: "8px",
-            }}
-          />
-          <input
-            type="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={{
-              padding: "8px",
-              border: "1px solid #ccc",
-              borderRadius: "4px",
-              marginRight: "8px",
-            }}
-          />
-          <br />
-          <br />
-          <button onClick={handleSignup}>Sign Up</button>
+      <div className="relative min-h-screen flex items-center justify-center">
+        {/* Background video */}
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute inset-0 h-full w-full object-cover z-0"
+        >
+          <source src="../public/Assets/Videos/video5.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        {/* Login Card */}
+        <div
+          className="relative z-10 h-[550px] w-[500px] overflow-hidden rounded-3xl bg-white bg-opacity-5
+        backdrop-blur-sm shadow-lg"
+        >
+          <div className="mt-3 space-y-8 px-10 py-10 text-center">
+            <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#3E2723] via-[#0D47A1] to-[#B71C1C] mb-6">
+              Nepali Radio
+            </h1>
+            <div className="group relative">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="peer h-14 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none"
+              />
+              <label className="absolute left-2 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 peer-placeholder-shown:top-2 peer-placeholder-shown:h-full peer-focus:-top-7 peer-focus:h-1/2 peer-focus:pl-0 peer-focus:text-white">
+                Email
+              </label>
+            </div>
+
+            <div className="group relative">
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="peer h-14 w-full rounded-3xl bg-gray-100 px-4 text-sm outline-none"
+              />
+              <label className="absolute left-2 top-0 flex h-full transform items-center pl-2 text-base transition-all duration-300 peer-placeholder-shown:top-2 peer-placeholder-shown:h-full peer-focus:-top-7 peer-focus:h-1/2 peer-focus:pl-0 peer-focus:text-white">
+                Password
+              </label>
+            </div>
+
+            <button
+              className="h-12 w-full rounded-3xl bg-blue-900 text-white transition-all duration-300 hover:bg-blue-800"
+              onClick={handleSignup}
+            >
+              Sign Up
+            </button>
+          </div>
+
+          <p className="text-center text-white gap-2">
+            Already have an account?{" "}
+            <a
+              onClick={isLogin}
+              className="font-semibold text-blue-300 hover:text-blue-800 cursor-pointer"
+            >
+              Login
+            </a>
+          </p>
+
+          {/* Google Signup */}
+          <a
+            onClick={() => firebase.signupWithGoogle()}
+            className="border-white-500 group mx-20 mb-4 mt-5 inline-flex h-12 w-[320px] items-center justify-center space-x-2 rounded-3xl border px-4 py-2 transition-colors duration-300 hover:border-blue-500 hover:bg-blue-500 focus:outline-none"
+          >
+            <i className="fa fa-google text-white"></i>
+            <span className="text-sm font-medium text-white">
+              Sign up with Google
+            </span>
+          </a>
+
+          {/* GitHub Signup */}
+          <a
+            href="#"
+            className="border-white-500 group mx-20 my-0 inline-flex h-12 w-[320px] items-center justify-center space-x-2 rounded-3xl border px-4 py-2 transition-colors duration-300 hover:border-black hover:bg-black focus:outline-none"
+          >
+            <i className="fa fa-twitter text-white"></i>
+            <span className="text-sm font-medium text-white">Github</span>
+          </a>
         </div>
       </div>
-      <p>Or</p>
-      <button onClick={() => firebase.signupWithGoogle()} target="blank">
-        Signup with Google
-      </button>
-      <br />
-      <br />
-      <span>
-        Already have an account?
-        <a onClick={isLogin} style={{ cursor: "pointer" }}>
-          {" "}
-          Login
-        </a>
-      </span>
     </>
   );
 }
